0a79f0d128499291c374d6851827e9c7af346eab
victor297 <davidvictor297.com>
2023-05-29 19:21:10 +0100

    openAI

---
+++ pages/api/generate-gifts.js
@@ -0,0 +1,54 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function (req, res) {
+  if (!configuration.apiKey) {
+    res.status(500).json({
+      error: {
+        message:
+          "OpenAI API key not configured, please follow instructions in README.md",
+      },
+    });
+    return;
+  }
+
+  const { priceMin, priceMax, gender, age, hobbies } = req.body || "";
+  //   if (boy.trim().length === 0) {
+  //     res.status(400).json({
+  //       error: {
+  //         message: "Please enter a valid boy",
+  //       },
+  //     });
+  //     return;
+  //   }
+
+  try {
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: generatePrompt(priceMin, priceMax, gender, age, hobbies),
+      temperature: 0.6,
+    });
+    res.status(200).json({ result: completion.data.choices[0].text });
+  } catch (error) {
+    // Consider adjusting the error handling logic for your use case
+    if (error.response) {
+      console.error(error.response.status, error.response.data);
+      res.status(error.response.status).json(error.response.data);
+    } else {
+      console.error(`Error with OpenAI API request: ${error.message}`);
+      res.status(500).json({
+        error: {
+          message: "An error occurred during your request.",
+        },
+      });
+    }
+  }
+}
+
+function generatePrompt(priceMin, priceMax, gender, age, hobbies) {
+  return `suggest 3 Christmas gift ideas between ${priceMin}$ and ${priceMax}$ for a ${age} years old ${gender} that is into ${hobbies}.`;
+}
